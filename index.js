e7d3892af27f1469a26772fdfea8a7f3fde8689b
jpedakal <jayakrishna490@gmail.com>
2025-09-21 19:47:02 +0530

    added logs

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const app = express();
 
 app.get("/fact/:id", (req, res) => {
   const params = req.params.id;
+  console.log('req', new Date(), params)
   let result = 1;
   for (let i = 1; i <= params; i++) {
     result *= i;
   }
+  console.log('response', new Date(), result)
   res.send(`Factorial of num ${params} is ${result}`);
 });
 
