73e672bd4f4f04ffad40db0f2cb2083689bfdba7
jpedakal <jayakrishna490@gmail.com>
2025-09-21 20:07:30 +0530

    updated logs

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
+const logInfo = require("./logs");
 
 app.get("/fact/:id", (req, res) => {
   const params = req.params.id;
-  console.log('req', new Date(), params)
+  logInfo.log("req", new Date(), params);
   let result = 1;
   for (let i = 1; i <= params; i++) {
     result *= i;
   }
-  console.log('response', new Date(), result)
+  logInfo.log("response", new Date(), result);
   res.send(`Factorial of num ${params} is ${result}`);
 });
 
---
+++ logs.js
@@ -0,0 +1,10 @@
+const { Logging } = require('@google-cloud/logging');
+const logging = new Logging();
+const log = logging.log('nodejs-app');
+
+function writeLog(message) {
+  const entry = log.entry({ resource: { type: 'gce_instance', labels: { instance_id: '8866099574853481230' } } }, message);
+  log.write(entry);
+}
+
+module.log = writeLog
