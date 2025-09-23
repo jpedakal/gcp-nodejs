fddf01294a6bc92e569956c3f19e5f4990b99335
jpedakal <jayakrishna490@gmail.com>
2025-09-24 00:52:19 +0530

    updated port

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ app.get("/fact/:id", async (req, res) => {
   res.send(`Factorial of num ${params} is ${result}`);
 });
 
-app.listen(80, "0.0.0.0", () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
